d866dceea27d508644e63515b81f043e8be9f2eb
John Wu <me@johnswu.net>
2024-09-28 19:10:46 -0700

    major progress with the prover and verifier implementation of bulletproofs with rust

--- src/bulletproofs/traits.rs
+++ src/bulletproofs/traits.rs
@@ -1,86 +1,66 @@
 use ark_ec::CurveGroup;
 use ark_ff::Field;
-use ark_ec::Group;
 
-
-pub trait BulletproofSystem<S: Field, G: CurveGroup<BaseField = S>> {
-    type Proof;
-    type Commitment;
-
-    fn new(max_bits: usize) -> Self;
-
-    // This should actually
-    fn prove(
-        &self,
-        // generators: BulletproofGenerators<G>,
-        v1: Vec<G>,
-        v2: Vec<G>,
-    ) -> (Self::Proof, Self::Commitment);
-
-    fn verify(&self, proof: &Self::Proof, commitment: &Self::Commitment) -> bool;
-}
-
-pub struct BulletproofGenerators<G: CurveGroup> {
+#[derive(Clone)]
+pub struct BulletproofGenerators<G: CurveGroup + Clone> {
     pub g: Vec<G>,
     pub h: Vec<G>,
-    pub u: G,
+    pub u: G, 
 }
 
-pub struct BulletproofProof<S: Field, G: CurveGroup<ScalarField = S>> {
+#[derive(Clone)]
+pub struct BulletproofRecProof<S: Field + Clone, G: CurveGroup<ScalarField = S> + Clone> {
     pub dot_product: S,
     pub pedersen_commitment: G, // This is the base commitment
     pub l_value: G,             // This is the left value in the proof
     pub r_value: G,             // This is the right value in the proof
 }
 
-pub struct BulletproofVerifierChallenge<S: Field> {
+#[derive(Clone)]
+pub struct BulletproofVerifierChallenge<S: Field + Clone> {
     pub random_challenge: S,
 }
 
+pub trait VerifierChallenger<S: Field + Clone, G: CurveGroup<ScalarField = S> + Clone> {
+    fn generate_challenge(&self, proof: &BulletproofRecProof<S, G>) -> S;
+}
+
+pub struct DefaultVerifierChallenger;
+
+impl<S: Field + Clone, G: CurveGroup<ScalarField = S> + Clone> VerifierChallenger<S, G> for DefaultVerifierChallenger {
+    fn generate_challenge(&self, proof: &BulletproofRecProof<S, G>) -> S {
+        // This is a placeholder implementation.
+        // In a real-world scenario, you would use a cryptographic hash function
+        // to generate the challenge based on the proof components.
+        // For now, we'll just return a dummy value.
+        S::from(1u64)
+    }
+}
+
+
 // You can show the entire two vectors if the number of elements is less than 2
-pub struct BulletproofProofSmall<S: Field, G: CurveGroup<ScalarField = S>> {
-    pub v1: Vec<G>,
-    pub v2: Vec<G>,
-    pub dot_product: S,
+pub struct BulletproofProofSmall<S: Field> {
+    pub value1: S,
+    pub value2: S,
+    pub dot_product: S
 }
 
-pub trait InteractiveBulletproofSystem<S: Field, G: CurveGroup<ScalarField = S>> {
-    fn prove_rec(
-        generators: BulletproofGenerators<G>,
-        v1: Vec<S>,
-        v2: Vec<S>,
-    ) -> BulletproofProof<S, G>;
-
-    fn prove_update(
-        challenge: BulletproofVerifierChallenge<S>,
-        generators: BulletproofGenerators<G>,
-        v1: Vec<S>,
-        v2: Vec<S>,
-    ) -> (BulletproofGenerators<G>, Vec<G>, Vec<G>);
-
-    // It's more like they verify everything in the end
-    fn verifier_rec(proof: BulletproofProof<S, G>) -> Option<BulletproofVerifierChallenge<S>>;
-
-    fn prove_small(
-        generators: BulletproofGenerators<G>,
-        v1: Vec<S>,
-        v2: Vec<S>,
-    ) -> BulletproofProofSmall<S, G>;
-
-    fn verifier_small(proof: BulletproofProofSmall<S, G>) -> bool;
+pub struct BulletproofProof<S: Field, G: CurveGroup<ScalarField = S>> {
+    pub rec_proofs: Vec<(BulletproofRecProof<S, G>, BulletproofVerifierChallenge<S>)>,
+    pub small_proof: BulletproofProofSmall<S>,
 }
 
-pub struct InteractiveBulletproof<S, G>(std::marker::PhantomData<(S, G)>);
 
-impl<S: Field, G: CurveGroup<ScalarField = S>> InteractiveBulletproofSystem<S, G>
-    for InteractiveBulletproof<S, G>
-{
-    fn prove_rec(
+
+
+mod prover {
+    use super::*;
+
+    fn prove_rec<S: Field, G: CurveGroup<ScalarField = S>>(
         generators: BulletproofGenerators<G>,
         v1: Vec<S>,
         v2: Vec<S>,
-    ) -> BulletproofProof<S, G> {
-
+    ) -> BulletproofRecProof<S, G> {
         let n = v1.len();
         assert_eq!(n, v2.len(), "Input vectors must have the same length");
 
@@ -92,17 +72,16 @@ impl<S: Field, G: CurveGroup<ScalarField = S>> InteractiveBulletproofSystem<S, G
         let (h_l, h_r) = generators.h.split_at(m);
 
         // Compute L = [<a_L, b_R>]U + [a_L]G_R + [b_R]H_L
-        let l_value = compute_commitment(a_l, b_r, g_r, h_l, &generators.u);
+        let l_value = compute_intermediate_commitment(a_l, b_r, &generators.u, g_r, h_l);
 
         // Compute R = [<a_R, b_L>]U + [a_R]G_L + [b_L]H_R
-        let r_value = compute_commitment(a_r, b_l, g_l, h_r, &generators.u);
+        let r_value = compute_intermediate_commitment(a_r, b_l, &generators.u, g_l, h_r);
 
-        // Compute dot product of entire v1 and v2
         let dot_product = compute_dot_product(&v1, &v2);
 
         let pedersen_commitment = compute_pedersen_commitment(&v1, &v2, dot_product, &generators.g, &generators.h, &generators.u);
 
-        BulletproofProof {
+        BulletproofRecProof {
             dot_product,
             pedersen_commitment,
             l_value,
@@ -110,77 +89,118 @@ impl<S: Field, G: CurveGroup<ScalarField = S>> InteractiveBulletproofSystem<S, G
         }
     }
 
-    fn prove_update(
-        challenge: BulletproofVerifierChallenge<S>,
-        generators: BulletproofGenerators<G>,
-        v1: Vec<S>,
-        v2: Vec<S>,
-    ) -> (BulletproofGenerators<G>, Vec<G>, Vec<G>) {
-        let x = challenge.random_challenge;
-        let x_inv = x.inverse().expect("Challenge should be non-zero");
-
-        // Update v1 and v2
-        let v1_new: Vec<S> = v1.iter().zip(v2.iter())
-            .map(|(a_i, b_i)| *a_i * x + *b_i * x_inv)
-            .collect();
-        let v2_new: Vec<S> = v1.iter().zip(v2.iter())
-            .map(|(a_i, b_i)| *a_i * x_inv + *b_i * x)
-            .collect();
-
-        // Update generators
-        let g_new: Vec<G> = generators.g.iter().zip(generators.h.iter())
-            .map(|(g_i, h_i)| g_i.mul(x_inv) + h_i.mul(x))
-            .collect();
-        let h_new: Vec<G> = generators.g.iter().zip(generators.h.iter())
-            .map(|(g_i, h_i)| g_i.mul(x) + h_i.mul(x_inv))
-            .collect();
-
-        // Update commitment
-        let u_new = generators.u.mul(x.square()) + generators.u.mul(x_inv.square());
-
-        let new_generators = BulletproofGenerators {
-            g: g_new,
-            h: h_new,
-            u: u_new,
-        };
-
-        (new_generators, v1_new.iter().map(|s| G::generator().mul(s)).collect(), v2_new.iter().map(|s| G::generator().mul(s)).collect())
-    }
-
-    fn verifier_rec(proof: BulletproofProof<S, G>) -> BulletproofVerifierChallenge<S> {
-        // Implement the recursive verification algorithm here
-        // This is a placeholder implementation
-        let random_challenge = S::zero(); // Generate actual random challenge
-
-        // Perform verification checks
-        let verification_passed = true; // Replace with actual verification logic
-
-        if verification_passed {
-            Some(BulletproofVerifierChallenge { random_challenge })
-        } else {
-            None
+    fn prove_small<S: Field, G: CurveGroup<ScalarField = S>>(
+        x1: S,
+        x2: S,
+    ) -> BulletproofProofSmall<S> {
+        BulletproofProofSmall {
+            value1: x1,
+            value2: x2,
+            dot_product: x1 * x2,
         }
     }
 
-    fn prove_small(
+    pub fn prove<S: Field, G: CurveGroup<ScalarField = S>>(
         generators: BulletproofGenerators<G>,
-        v1: Vec<G>,
-        v2: Vec<G>,
-    ) -> BulletproofProofSmall<S, G> {
-        todo!()
-    }
+        v1: Vec<S>,
+        v2: Vec<S>,
+    ) -> BulletproofProof<S, G> {
+        fn prove_recursive<S: Field, G: CurveGroup<ScalarField = S>>(
+            generators: BulletproofGenerators<G>,
+            v1: Vec<S>,
+            v2: Vec<S>,
+            mut rec_proofs: Vec<(BulletproofRecProof<S, G>, BulletproofVerifierChallenge<S>)>,
+        ) -> BulletproofProof<S, G> {
+            if v1.len() <= 2 {
+                let small_proof = if v1.len() == 2 {
+                    prove_small(v1[0], v2[0])
+                } else {
+                    prove_small(v1[0], v2[0])
+                };
+                BulletproofProof {
+                    rec_proofs,
+                    small_proof,
+                }
+            } else {
+                let rec_proof = prove_rec(generators.clone(), v1.clone(), v2.clone());
+                let challenge = super::verifier_rec(rec_proof.clone()).expect("Verification failed");
+                rec_proofs.push((rec_proof, challenge.clone()));
 
-    fn verifier_small(proof: BulletproofProofSmall<S, G>) -> bool {
-        todo!()
+                let (new_generators, new_v1, new_v2) = super::prove_update(challenge, generators, v1, v2);
+
+                prove_recursive(new_generators, new_v1, new_v2, rec_proofs)
+            }
+        }
+
+        prove_recursive(generators, v1, v2, Vec::new())
     }
 }
 
-fn compute_commitment<S: Field, G: CurveGroup<ScalarField = S>>(
+mod verifier {
+    use super::*;
+
+    pub fn verify<S: Field, G: CurveGroup<ScalarField = S>>(proof: BulletproofProof<S, G>, generators: BulletproofGenerators<G>) -> bool {
+        let current_proof = proof;
+        let mut current_generators = generators;
+
+        for i in 0..(current_proof.rec_proofs.len() - 1) {
+            let (rec_proof, challenge) = &current_proof.rec_proofs[i];
+            let next_commitment = &current_proof.rec_proofs[i + 1].0.pedersen_commitment;
+            let verification_passed = verify_rec(rec_proof, challenge, next_commitment);
+            if !verification_passed {
+                return false;
+            }
+
+            current_generators = update_generators(&current_generators, challenge.random_challenge);
+        }
+
+        let small_proof = &current_proof.small_proof;
+        let final_commitment = &current_proof.rec_proofs.last().unwrap().0.pedersen_commitment;
+
+        verify_small(&small_proof, &current_generators, final_commitment)
+    }
+
+    fn verify_rec<S: Field, G: CurveGroup<ScalarField = S>>(
+        proof: &BulletproofRecProof<S, G>,
+        challenge: &BulletproofVerifierChallenge<S>,
+        next_commitment: &G
+    ) -> bool {
+        let x = challenge.random_challenge;
+        let x_inv = x.inverse().expect("Challenge should be non-zero");
+    
+        // Compute the new commitment using the proof values and the challenge
+        let computed_commitment = proof.l_value.mul(x.square())
+            + proof.r_value.mul(x_inv.square())
+            + proof.pedersen_commitment;
+    
+        // Check if the computed commitment matches the next commitment in the chain
+        computed_commitment == *next_commitment
+    }
+
+    fn verify_small<S: Field, G: CurveGroup<ScalarField = S>>(proof: &BulletproofProofSmall<S>, generators: &BulletproofGenerators<G>, final_commitment: &G) -> bool {
+        // make sure the generators are of only size 1
+        assert!(generators.g.len() == 1 && generators.h.len() == 1);
+
+        let g_value = generators.g[0];
+        let h_value = generators.h[0];
+
+        let computed_commitment = g_value.mul(proof.value1) + h_value.mul(proof.value2) + generators.u.mul(proof.dot_product);
+
+        computed_commitment == *final_commitment
+    }
+}
+
+
+
+
+// Compute: u * <a, b> + <a, g> + <b, h>
+// where <x, y> denotes the dot product or multi-scalar multiplication
+fn compute_intermediate_commitment<S: Field, G: CurveGroup<ScalarField = S>>(
     a: &[S],
     b: &[S],
+    u: &G,
     g: &[G],
     h: &[G],
-    u: &G,
 ) -> G {
     let ab_dot = compute_dot_product(a, b);
     let ag = multi_scalar_mul(a, g);
@@ -196,6 +216,7 @@ fn compute_dot_product<S: Field>(a: &[S], b: &[S]) -> S {
 }
 
 fn multi_scalar_mul<S: Field, G: CurveGroup<ScalarField = S>>(scalars: &[S], points: &[G]) -> G {
+    assert_eq!(scalars.len(), points.len(), "Scalars and points must have the same length");
     scalars.iter()
         .zip(points.iter())
         .map(|(s, p)| p.mul(*s))
@@ -211,4 +232,54 @@ fn compute_pedersen_commitment<S: Field, G: CurveGroup<ScalarField = S>>(
     u: &G,
 ) -> G {
     multi_scalar_mul(v1, g) + multi_scalar_mul(v2, h) + u.mul(&dot_product)
+}
+
+fn update_generators<S: Field, G: CurveGroup<ScalarField = S>>(
+    generators: &BulletproofGenerators<G>,
+    x: S,
+) -> BulletproofGenerators<G> {
+    let n = generators.g.len();
+    let m = n / 2;
+    let x_inv = x.inverse().expect("Challenge should be non-zero");
+
+    let g_new: Vec<G> = generators.g[..m].iter().zip(generators.g[m..].iter())
+        .map(|(g_l, g_r)| g_l.mul(x_inv) + g_r.mul(x))
+        .collect();
+    let h_new: Vec<G> = generators.h[..m].iter().zip(generators.h[m..].iter())
+        .map(|(h_l, h_r)| h_l.mul(x) + h_r.mul(x_inv))
+        .collect();
+    let u_new = generators.u;  // U doesn't change
+
+    BulletproofGenerators {
+        g: g_new,
+        h: h_new,
+        u: u_new,
+    }
+}
+
+pub fn prove_update<S: Field, G: CurveGroup<ScalarField = S>>(
+    challenge: BulletproofVerifierChallenge<S>,
+    generators: BulletproofGenerators<G>,
+    v1: Vec<S>,
+    v2: Vec<S>,
+) -> (BulletproofGenerators<G>, Vec<S>, Vec<S>) {
+    let x = challenge.random_challenge;
+    let x_inv = x.inverse().expect("Challenge should be non-zero");
+
+    let n = v1.len();
+    assert_eq!(n, v2.len(), "Input vectors must have the same length");
+    let m = n / 2;
+
+    // Update v1 and v2
+    let v1_new: Vec<S> = v1[..m].iter().zip(v1[m..].iter())
+        .map(|(a_l, a_r)| *a_l * x_inv + *a_r * x)
+        .collect();
+    let v2_new: Vec<S> = v2[..m].iter().zip(v2[m..].iter())
+        .map(|(b_l, b_r)| *b_l * x + *b_r * x_inv)
+        .collect();
+
+    // Update generators
+    let new_generators = update_generators(&generators, x);
+
+    (new_generators, v1_new, v2_new)
 }
